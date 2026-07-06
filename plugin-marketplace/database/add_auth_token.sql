9d501d12c1df297b926dd011ee7ef901562bebe9
michaelkershaw <michaelkershaw-son@hotmail.co.uk>
2026-07-06 18:11:37 +0100

    Delete plugin-marketplace directory

--- plugin-marketplace/database/add_auth_token.sql
+++
@@ -1,6 +0,0 @@
--- Add auth_token column to licenses table for account-based authentication
-ALTER TABLE licenses ADD COLUMN auth_token VARCHAR(128) NULL AFTER license_key;
-ALTER TABLE licenses ADD COLUMN last_login_at DATETIME NULL AFTER auth_token;
-
--- Add index for faster auth token lookups
-ALTER TABLE licenses ADD INDEX idx_auth_token (auth_token);
--- plugin-marketplace/includes/admin-footer.php
+++
@@ -1,6 +0,0 @@
-<?php if (!defined('BASE_PATH')) exit; ?>
-    </main>
-</div>
-</div>
-</body>
-</html>
